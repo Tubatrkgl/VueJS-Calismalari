270a092cbe6ce96d461b22a6839ca5048f096739
Tuba Türkoğlu <92tuba92@gmail.com>
2024-01-21 15:31:25 +0300

    09-Vue-Router

---
+++ 09-Vue-Router/router-app/src/views/StudentsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Öğrenciler</h1>
+    <div v-for="student in students" :key="student.id">
+    <p>{{student.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      students: [
+        { id: 1, name: "Varol", not: 55 },
+        { id: 2, name: "Ali", not: 75 },
+        { id: 3, name: "Mehmet", not: 85 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
