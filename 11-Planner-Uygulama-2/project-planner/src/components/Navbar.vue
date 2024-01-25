e6670fa57c762007e94917e1a88b9ab112180094
Tuba Türkoğlu <92tuba92@gmail.com>
2024-01-25 15:12:44 +0300

    navbar.vue , addproject.vue

---
+++ 11-Planner-Uygulama-2/project-planner/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="main">
+    <router-link :to="{ name: 'Home' }"></router-link>
+    <router-link :to="{ name: 'Home' }"></router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+</style>
