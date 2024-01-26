88c58a3e3d4fdaa3d714432ec296d4e4c49fe96d
Tuba Türkoğlu <92tuba92@gmail.com>
2024-01-26 22:56:51 +0300

    app.vue , filternav.vue, navbar.vue, editproject.vue

--- 11-Planner-Uygulama-2/project-planner/src/components/Navbar.vue
+++ 11-Planner-Uygulama-2/project-planner/src/components/Navbar.vue
@@ -1,15 +1,29 @@
 <template>
   <nav class="main">
-    <router-link :to="{ name: 'Home' }"></router-link>
-    <router-link :to="{ name: 'Home' }"></router-link>
+    <router-link :to="{ name: 'Home' }">Projects</router-link> |
+    <router-link :to="{ name: 'AddProject' }">Add New Project</router-link>
   </nav>
 </template>
 
 <script>
-export default {
-    
-};
+export default {};
 </script>
 
 <style>
+nav.main {
+  text-align: center;
+  margin: 40px auto;
+}
+nav.main a {
+  display: inline-block;
+  text-decoration: none;
+  color: #999;
+  margin: 0 10px;
+  font-size: 18px;
+}
+ a.router-link-active{
+  border-bottom: 2px solid #76dd78;
+  padding-bottom:4px;
+  color: #76dd78;
+ }
 </style>
