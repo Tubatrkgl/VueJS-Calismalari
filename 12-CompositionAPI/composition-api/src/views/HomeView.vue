378bc55e649e4ea2e9a10f3d9d44030db0afc668
Tuba Türkoğlu <92tuba92@gmail.com>
2024-02-03 01:03:40 +0300

    composition-apiI

---
+++ 12-CompositionAPI/composition-api/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+  <p>{{name}}-{{job}}</p>
+  <button @click="clicked">Click</button>
+
+  </div>
+</template>
+
+<script>
+import onMounted from "vue";
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    let name = "Varol";
+    let job = "Çaycı";
+
+    const clicked =() =>{
+      console.log("Buton tıklandı");
+    };
+    return{name,job, clicked}
+  },
+};
+</script>
