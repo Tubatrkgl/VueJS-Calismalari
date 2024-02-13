aae4f6ac59f8ec603050c2b37e959f1eeaf43d04
Tuba Türkoğlu <92tuba92@gmail.com>
2024-02-13 23:09:54 +0300

    ref, refile,reactive setup computed, watch watch effect

--- 12-CompositionAPI/composition-api/src/views/HomeView.vue
+++ 12-CompositionAPI/composition-api/src/views/HomeView.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="home">
-  <p>{{name}}-{{job}}</p>
-  <button @click="clicked">Click</button>
-
+    <input type="text" v-model="search" />
+    <div v-for="day in found" :key="day">
+      {{ day }}
+    </div>
+    <button @click='stop'>Stop</button>
   </div>
 </template>
 
 <script>
-import onMounted from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
+
 export default {
   name: "HomeView",
-  components: {},
   setup() {
-    let name = "Varol";
-    let job = "Çaycı";
+    const days = ref(["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma"]);
+    const search = ref("");
 
-    const clicked =() =>{
-      console.log("Buton tıklandı");
+    const found = computed(() => {
+      return days.value.filter((day) => day.includes(search.value));
+    });
+
+    
+    const watchSearch = watch(search, () => {
+      console.log("watch çalıştı");
+    });
+
+    const watchEffectDays = watchEffect(() => {
+      console.log('watchEffect çalıştı', days.value);
+      console.log('watchEffect çalıştı', search.value);
+    });
+
+    const stop = () => {
+     
+      watchSearch();
+      watchEffectDays();
     };
-    return{name,job, clicked}
+
+    return { days, search, found, stop };
   },
 };
 </script>
